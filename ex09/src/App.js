231ea9639ba33696f45e19b52d49e7e860cfd2d4
Mirela <adisserdarevic@gmail.com>
2021-04-02 20:14:27 +0200

    Added folder ex09

---
+++ ex09/src/App.js
@@ -0,0 +1,11 @@
+//App.js
+import "./App.css";
+import PutRequest from "./PutRequest";
+function App() {
+  return (
+    <div className="App">
+      <PutRequest />
+    </div>
+  );
+}
+export default App;
