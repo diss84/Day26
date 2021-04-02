06eb40a6b60306d8af7e52146daf9a680127f407
Mirela <adisserdarevic@gmail.com>
2021-04-02 20:13:51 +0200

    Added folder ex08

---
+++ ex08/src/App.js
@@ -0,0 +1,11 @@
+//App.js
+import "./App.css";
+import PostRequestSetHeaders from "./PostRequestSetHeaders";
+function App() {
+  return (
+    <div className="App">
+      <PostRequestSetHeaders />
+    </div>
+  );
+}
+export default App;
