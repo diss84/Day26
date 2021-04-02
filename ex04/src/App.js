2b488e131b4192807567a31de79079a6e03572de
Mirela <adisserdarevic@gmail.com>
2021-04-02 20:10:41 +0200

    Added folder ex04

---
+++ ex04/src/App.js
@@ -0,0 +1,11 @@
+//App.js
+import "./App.css";
+import GetRequestSetHeaders from "./GetRequestSetHeaders";
+function App() {
+  return (
+    <div className="App">
+      <GetRequestSetHeaders />
+    </div>
+  );
+}
+export default App; //export
