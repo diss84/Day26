e767d10d8079fd19f68b3be8c02afb41642725df
Mirela <adisserdarevic@gmail.com>
2021-04-02 20:12:13 +0200

    Added folder ex06

---
+++ ex06/src/App.js
@@ -0,0 +1,11 @@
+//App.js
+import "./App.css";
+import PostRequestAsyncAwait from "./PostRequestAsyncAwait";
+function App() {
+  return (
+    <div className="App">
+      <PostRequestAsyncAwait />
+    </div>
+  );
+}
+export default App;
