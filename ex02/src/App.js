e7c6d6a4637870d258b7507274c3d1ecb4dcd2ff
Mirela <adisserdarevic@gmail.com>
2021-04-02 20:09:00 +0200

    Added folder ex02

---
+++ ex02/src/App.js
@@ -0,0 +1,11 @@
+//App.js
+import "./App.css";
+import GetRequestAsyncAwait from "./GetRequestAsyncAwait";
+function App() {
+  return (
+    <div className="App">
+      <GetRequestAsyncAwait />
+    </div>
+  );
+}
+export default App;
